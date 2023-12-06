6de307e8cb2720002c93c2b81cd8e44361646168
PradaWilly <112588780+PradaWilly@users.noreply.github.com>
2023-12-06 19:20:56 +0000

    Add files via upload
    added nav bar

--- script/javascript.js
+++ script/javascript.js
@@ -2,24 +2,24 @@
   function post() {
     var fname = document.querySelector('.fnameb').value.trim();
     var lname = document.querySelector('.snameb').value.trim();
-    var email = document.querySelector('.cremail2').value.trim();
-    var dob = document.querySelector('.crdob').value.trim();
+    var email = document.querySelector('.cremail2').value.trim();  //this grabs the information a user inputs in the form//
+    var dob = document.querySelector('.crdob').value.trim();       
     
     if (fname == '' || lname == '' || email == '' || dob == '') {
-      alert('Please fill in all required fields.');
+      alert('Please fill in all required fields.');            //this is validation, so it checks if all the fields have been filled//
       return;
     }
 
     var interest = document.querySelector('input[name="interest"]:checked');
     var notification = document.querySelector('input[name="notis"]:checked');
-    var interestValue = interest ? interest.value : 'Not selected';
+    var interestValue = interest ? interest.value : 'Not selected';        //this checks if the radio boxes have been ticked or not//
     var notificationValue = notification ? notification.value : 'Not selected';
 
     
     var msg='Here is your information!' + '\n' +
                 'First Name: ' + fname + '\n' +
                 'Last Name: ' + lname + '\n' +
-                'Email: ' + email + '\n' +
+                'Email: ' + email + '\n' +       //this alerts the information that the script grabbed//
                 'Date of Birth: ' + dob + '\n' +
                 'What are you most interested in ?: ' + interestValue + '\n' +
                 'Would you like to receive notifications ?: ' + notificationValue;
